b96a456c3034250279d61705119fab5e65e778e9
manoj <manojmulammagari@gmail.com>
2026-05-24 18:44:46 +0530

    Improve isPalindrome function for valid input handling
    Refactor the isPalindrome function to handle non-alphanumeric characters.

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,7 +3,6 @@ class Solution:
        left,right=0,len(s)-1
 
        while left < right  :
-
         while left<right and not s[left].isalnum():
             left+=1
         while left<right and not s[right].isalnum():
@@ -15,7 +14,3 @@ class Solution:
         left+=1
         right-=1
        return True
-
-
-
-            
