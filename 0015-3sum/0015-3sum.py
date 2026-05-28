36d6a2a095bda324b43727fc820b61a6c726295b
manoj <manojmulammagari@gmail.com>
2026-05-28 08:41:45 +0530

    Time: 588 ms (75.11%), Space: 22.3 MB (54.46%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,22 +4,22 @@ class Solution:
         b = []
 
         for i,n in enumerate(nums):
-            if i>0 and n==nums[i-1]:
+            if i>0 and nums[i]==nums[i-1]:
                 continue
             
             l,r=i+1,len(nums)-1
 
             while l<r:
                 v=n+nums[l]+nums[r]
-                if v==0:
-                    b.append([n,nums[l],nums[r]])
-                    l+=1
-                    while nums[l]==nums[l-1] and l<r:
-                        l+=1
+                if v>0:
+                    r-=1
                 elif v<0:
                     l+=1
                 
                 else :
-                    r-=1
+                    b.append([n,nums[l],nums[r]])
+                    l+=1
+                    while nums[l]==nums[l-1] and l<r:
+                        l+=1
 
         return b
