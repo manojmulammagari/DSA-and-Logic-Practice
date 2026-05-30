948c48d671350dc0948d55f3059475f6f10442c2
manoj <manojmulammagari@gmail.com>
2026-05-30 06:54:13 +0530

    Time: 11 ms (77.03%), Space: 19.2 MB (92.9%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -6,18 +6,16 @@ class Solution:
         left=0
 
         ml=0
-        
 
         for i,n in enumerate(s):
-            while n in c and c[n]>=left:
-                left=c[n]+1
 
+            while n in c and c[n]>=left:
+
+                left=c[n]+1
 
             c[n]=i
 
-
             ml=max(ml,i-left+1)
-
 
         return ml
     
