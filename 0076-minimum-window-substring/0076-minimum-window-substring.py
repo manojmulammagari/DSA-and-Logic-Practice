2dd735b6dbc01489b6bba6f49e8d99caef6ed5ff
manoj <manojmulammagari@gmail.com>
2026-06-09 07:42:24 +0530

    Time: 55 ms (92.24%), Space: 19.8 MB (37.61%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -23,16 +23,19 @@ class Solution:
         for right in range(len(s)):
 
             cr = s[right]
-            
+
             window[cr] = window.get(cr, 0) + 1
 
             if cr in tar_cou and window[cr] == tar_cou[cr]:
+
                 form_char = form_char + 1
 
             while form_char == uni:
+
                 curlen = right - left + 1
 
                 if curlen < min_len:
+
                     min_len = curlen
                     bestleft = left
                     bestright = right
@@ -42,12 +45,15 @@ class Solution:
                 window[charl] = window.get(charl, 0) - 1
 
                 if charl in tar_cou and window[charl] < tar_cou[charl]:
+
                     form_char = form_char - 1
 
                 left = left + 1
 
         if min_len == len(s) + 1:
+
             return ""
 
         else:
+            
             return s[bestleft : bestright + 1]
