4a5fe62dd8bb2abf73a39507fa3a5f68f70c5f15
manoj <manojmulammagari@gmail.com>
2026-06-09 07:42:00 +0530

    Time: 73 ms (51.15%), Space: 19.8 MB (37.61%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,7 +1,7 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
-        if s is "" or t is "" or len(s)<len(t):
+
+        if s is "" or t is "" or len(s) < len(t):
             return ""
 
         tar_cou = Counter(t)
@@ -14,21 +14,23 @@ class Solution:
 
         form_char = 0
 
-        min_len = len(s)+1
+        min_len = len(s) + 1
 
         bestleft = 0
 
         bestright = 0
 
         for right in range(len(s)):
+
             cr = s[right]
-            window[cr] = window.get(cr,0)+1
+            
+            window[cr] = window.get(cr, 0) + 1
 
             if cr in tar_cou and window[cr] == tar_cou[cr]:
                 form_char = form_char + 1
 
             while form_char == uni:
-                curlen = right - left +1
+                curlen = right - left + 1
 
                 if curlen < min_len:
                     min_len = curlen
@@ -37,14 +39,14 @@ class Solution:
 
                 charl = s[left]
 
-                window[charl] = window.get(charl,0)-1
+                window[charl] = window.get(charl, 0) - 1
 
-                if charl in tar_cou and window[charl]<tar_cou[charl]:
+                if charl in tar_cou and window[charl] < tar_cou[charl]:
                     form_char = form_char - 1
 
                 left = left + 1
 
-        if min_len == len(s)+1:
+        if min_len == len(s) + 1:
             return ""
 
         else:
