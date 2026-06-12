eebcbb35cfc2004ab5048b0436260f8da13be811
manoj <manojmulammagari@gmail.com>
2026-06-12 10:30:37 +0530

    Time: 69 ms (57.42%), Space: 19.8 MB (12.44%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,47 +2,36 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
 
         if s == "" or t == "" or len(s) < len(t):
-
             return ""
 
-        t_count = Counter(t)
-        window = {}
-        unique = len(t_count)
-        formed = 0
-        left = 0
-        bestleft = 0
-        bestright = 0
-        minimum = len(s) + 1
+        counT, window = Counter(t), {}
 
-        for right in range(len(s)):
-            ch = s[right]
+        have, need = 0, len(counT)
 
-            window[ch] = window.get(ch, 0) + 1
+        l = 0
+        res, reslen = [-1, -1], float("infinity")
 
-            if ch in t_count and window[ch] == t_count[ch]:
-                formed += 1
+        for r in range(len(s)):
+            c = s[r]
 
-            while formed == unique:
+            window[c] = 1 + window.get(c, 0)
 
-                current_length = right - left + 1
+            if c in counT and window[c] == counT[c]:
+                have += 1
 
-                if current_length < minimum:
-                    minimum = current_length
-                    bestleft = left
-                    bestright = right
+            while have == need:
+                v = r - l + 1
 
-                char_left = s[left]
+                if v < reslen:
+                    reslen = v
 
-                window[char_left] = window.get(char_left, 0) - 1
+                    res = [l, r]
 
-                if char_left in t_count and window[char_left] < t_count[char_left]:
+                window[s[l]] -= 1
 
-                    formed -= 1
+                if s[l] in counT and window[s[l]] < counT[s[l]]:
+                    have -= 1
 
-                left += 1
+                l += 1
 
-        if minimum == len(s)+1:
-            return ""
-
-        else:
-            return s[bestleft:bestright +1]
+        return s[res[0] : res[1] + 1] if reslen != float("infinity") else ""
