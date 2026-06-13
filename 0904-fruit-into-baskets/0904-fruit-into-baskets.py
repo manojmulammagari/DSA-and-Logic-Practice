d42443f98501b338306f1d1d71baf1fba6c9e45a
manoj <manojmulammagari@gmail.com>
2026-06-13 07:59:59 +0530

    Time: 184 ms (63.8%), Space: 25.9 MB (34.4%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -5,28 +5,28 @@ class Solution:
         m = 0
         window = {}
 
-        have = 0 
+        have = 0
 
         for r in range(len(fruits)):
 
             c = fruits[r]
             # Add the current fruit to our window frequency map
             window[c] = 1 + window.get(c, 0)
-            
+
             # If we have more than 2 types of fruit, shrink the window from the left
             while len(window) > 2:
                 left_fruit = fruits[l]
                 window[left_fruit] -= 1
-                
+
                 # CRITICAL: If the fruit count drops to 0, completely remove the key
                 if window[left_fruit] == 0:
                     del window[left_fruit]
-                
+
                 # Move the left pointer forward
                 l += 1
-            
+
             # Update the maximum length of a valid window
             v = r - l + 1
             m = max(m, v)
-        
+
         return m
