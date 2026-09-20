d5280267829d025416819e40e7c05119c493cb01
manoj <manojmulammagari@gmail.com>
2026-09-20 21:46:56 +0530

    Time: 19 ms (8.31%), Space: 19.3 MB (55.44%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,19 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-
         if x < 0:
-
             return False
             
         original = x
         rev = 0
-        
-        # Pop the last digit off 'x' and push it onto the back of 'rev'
         while x > 0:
                 
             digit = x % 10
             rev = (rev * 10) + digit
             x = x // 10
         
-        # Compare the mathematically reversed number to the original
         return original == rev
